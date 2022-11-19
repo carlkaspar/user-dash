4322c553b9c421523ca4a9b9a7bb3fdb040385ad
WEBMEDIA\Kasparp <kaspar.puhm@nortal.com>
2022-11-19 15:43:25 +0200

    feat: display alternative avatar when avatar url errors out

--- src/app/common/components/avatar/avatar.component.ts
+++ src/app/common/components/avatar/avatar.component.ts
@@ -3,11 +3,15 @@ import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
 @Component({
   selector: 'app-avatar',
   template: `
-    <img [src]="src" class="avatar-image">
+    <img [src]="src" (error)="setAlternativeImage()" class="avatar-image">
   `,
   styleUrls: ['./avatar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AvatarComponent {
   @Input() src: string;
+
+  setAlternativeImage() {
+    this.src = 'assets/images/brand.png';
+  }
 }
