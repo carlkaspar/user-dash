ce088c584f691ea30f32e00c5a6ce635dd76658b
WEBMEDIA\Kasparp <kaspar.puhm@nortal.com>
2022-11-19 16:44:10 +0200

    feat: layout enhancements

--- src/app/user-dashboard/components/user-dashboard/user-dashboard.component.ts
+++ src/app/user-dashboard/components/user-dashboard/user-dashboard.component.ts
@@ -4,7 +4,11 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
   selector: 'app-user-dashboard',
   template: `
     <div class="dashboard-header">
-
+      <h2 class="title">Account users</h2>
+      <div class="header-right">
+        <app-search></app-search>
+        <button app-button-primary>Connect users</button>
+      </div>
     </div>
     <app-user-list></app-user-list>
   `,
