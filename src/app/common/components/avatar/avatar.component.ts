59d370f4814c0f287061a4182be30c5818c02986
WEBMEDIA\Kasparp <kaspar.puhm@nortal.com>
2022-11-20 22:21:41 +0200

    feat: display brand avatar before real image loaded

--- src/app/common/components/avatar/avatar.component.ts
+++ src/app/common/components/avatar/avatar.component.ts
@@ -1,17 +1,22 @@
-import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from "@angular/core";
 
 @Component({
   selector: 'app-avatar',
   template: `
-    <img [src]="src" (error)="setAlternativeImage()" class="avatar-image">
+    <img *ngIf="!loaded" [src]="'assets/images/brand.png'" class="avatar-image">
+    <img [ngClass]="{'hidden': !loaded}" [src]="src"  class="avatar-image" (load)="onLoaded()">
   `,
   styleUrls: ['./avatar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AvatarComponent {
   @Input() src: string;
+  loaded = false;
 
-  setAlternativeImage() {
-    this.src = 'assets/images/brand.png';
+  constructor(private cd: ChangeDetectorRef) {}
+
+  onLoaded() {
+    this.loaded = true;
+    this.cd.detectChanges();
   }
 }
