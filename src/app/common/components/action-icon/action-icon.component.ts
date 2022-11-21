0065f69cabbc063cc8cdb4f81f674a7c66c1a568
WEBMEDIA\Kasparp <kaspar.puhm@nortal.com>
2022-11-22 01:41:41 +0200

    feat: add animation for action icon edit button

--- src/app/common/components/action-icon/action-icon.component.ts
+++ src/app/common/components/action-icon/action-icon.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { ChangeDetectionStrategy, Component, ElementRef, Input, OnInit, Renderer2 } from "@angular/core";
+import {fromEvent, takeUntil} from "rxjs";
+import {Destroyable} from "../../utils/destroyable";
 
 @Component({
   selector: 'button[app-action-icon]',
@@ -11,11 +13,28 @@ import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
   styleUrls: ['./action-icon.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ActionIconComponent {
+export class ActionIconComponent extends Destroyable implements OnInit {
   @Input() iconName: 'trash' | 'pencil';
   @Input() title: string;
 
   get iconPath() {
     return `assets/sprites/sprite.svg#${this.iconName}`
+  }
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {
+    super();
+  }
+
+  ngOnInit(): void {
+    if (this.iconName === 'pencil') {
+      fromEvent(this.elRef.nativeElement, 'click').pipe(
+        takeUntil(this.destroyed$)
+      ).subscribe(() => {
+        this.renderer.addClass(this.elRef.nativeElement, 'rotate');
+        setTimeout(() => {
+          this.renderer.removeClass(this.elRef.nativeElement, 'rotate');
+        }, 200)
+      });
+    }
   }
 }
