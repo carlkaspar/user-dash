0b79c1f2d0c43d150c44efec3cb2c5f0a1dccb16
WEBMEDIA\Kasparp <kaspar.puhm@nortal.com>
2022-11-22 19:32:41 +0200

    chore: configure json-server for heroku

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('./mock-data/users.json')
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`)
+})
+
 app.use(express.static(__dirname + '/dist/user-dash'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
